232735be54a4187d0e97f3394c57404d35c565cd
WitoldFracek <witek.fracek@gmail.com>
2025-12-03 09:13:23 +0100

    day 2 sol a done

---
+++ day2/day2.py
@@ -0,0 +1,41 @@
+from qwlist import Lazy
+from utils.file import read_lines
+from typing import Tuple
+
+
+def split(s: str) -> Tuple[str, str]:
+    middle = len(s) // 2 + len(s) % 2
+    return s[:middle], s[middle:]
+
+def sol_a() -> int:
+    res = (
+        Lazy(read_lines('input.txt'))
+        .flatmap(lambda s: s.split(','))
+        .filter(lambda s: len(s) > 0)
+        .map(lambda s: s.split('-'))
+        .map(lambda pair: (int(pair[0]), int(pair[1])))
+        .map(lambda pair: Lazy(i for i in range(pair[0], pair[1] + 1)))
+        .map(lambda xs: xs.map(str)
+             .map(split)
+             .filter(lambda pair: pair[0] == pair[1])
+             .map(''.join)
+             .map(int)
+             .sum()
+        )
+        .filter(lambda elem: elem is not None)
+        .sum()
+    )
+    return res
+
+
+def sol_b() -> int:
+    return -1
+
+
+def main():
+    print(f'Solution a: {sol_a()}')
+    print(f'Solution b: {sol_b()}')
+
+
+if __name__ == '__main__':
+    main()
